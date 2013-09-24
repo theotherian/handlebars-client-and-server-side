3a9dd53cd28d57f69b8097550b36a1d7de6d8c19
theotherian <ian.g.simpson@gmail.com>
2013-09-23 22:09:35 -0600

    unnecessary put

--- src/main/java/com/theotherian/handlebars/MessagesResource.java
+++ src/main/java/com/theotherian/handlebars/MessagesResource.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.Options;
@@ -22,12 +20,6 @@ public class MessagesResource implements Helper<String> {
   @Produces(MediaType.APPLICATION_JSON)
   public List<Message> get(@PathParam("name") String name) {
     return getMessages(name);
-  }
-  
-  @PUT
-  @Path("{name}")
-  public Response put(@PathParam("name") String name, List<Message> messages) {
-    return Response.ok().build();
   }
   
   private List<Message> getMessages(String name) {
